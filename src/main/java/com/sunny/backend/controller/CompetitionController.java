311cd3a02c2f0734aa7c39035939bc46a3390924
tkfdkskarl56 <tkfdkskarl56@naver.com>
2023-09-16 19:56:48 +0900

    swagger 정리

--- src/main/java/com/sunny/backend/controller/CompetitionController.java
+++ src/main/java/com/sunny/backend/controller/CompetitionController.java
@@ -6,6 +6,9 @@ import com.sunny.backend.dto.request.CompetitionRequestDto;
 import com.sunny.backend.dto.response.CompetitionResponseDto;
 import com.sunny.backend.security.userinfo.CustomUserPrincipal;
 import com.sunny.backend.service.CompetitionService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "3. Competition", description = "Competition API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/compettion")
@@ -22,18 +26,21 @@ public class CompetitionController {
 
     private final CompetitionService competitionService;
 
+    @ApiOperation(tags = "3. Competition", value = "친구에게 경쟁 신청")
     @PostMapping("")
     public ResponseEntity<CommonResponse.GeneralResponse> applyCompetition(@AuthUser CustomUserPrincipal customUserPrincipal,
                                              @RequestBody CompetitionRequestDto.CompetitionApply competitionApply) {
         return ResponseEntity.ok(competitionService.applyCompetition(customUserPrincipal, competitionApply));
     }
 
+    @ApiOperation(tags = "3. Competition", value = "경쟁 승인하기")
     @PostMapping("/accept")
     public ResponseEntity<CommonResponse.GeneralResponse> acceptCompetition(@AuthUser CustomUserPrincipal customUserPrincipal,
         @RequestBody CompetitionRequestDto.CompetitionAccept competitionAccept) {
         return ResponseEntity.ok(competitionService.acceptCompetition(customUserPrincipal, competitionAccept));
     }
 
+    @ApiOperation(tags = "3. Competition", value = "경쟁 상태 가져오기")
     @GetMapping("/status/{competition_id}")
     public ResponseEntity<CommonResponse.SingleResponse<CompetitionResponseDto.CompetitionStatus>> getCompetitionStatus(
         @AuthUser CustomUserPrincipal customUserPrincipal, @PathVariable(name = "competition_id") Long id) {
