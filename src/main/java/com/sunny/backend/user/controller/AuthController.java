8fe7b5106b6a0b8e220b3d82be53c7409c8d2f3e
tkfdkskarl56 <tkfdkskarl56@naver.com>
2023-09-16 19:56:48 +0900

    swagger 정리

--- src/main/java/com/sunny/backend/user/controller/AuthController.java
+++ src/main/java/com/sunny/backend/user/controller/AuthController.java
@@ -6,23 +6,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sunny.backend.common.CommonResponse;
+import com.sunny.backend.common.ResponseService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "0. User", description = "User API")
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
 
+	private final ResponseService responseService;
+
+	@ApiOperation(tags = "0. User", value = "http://localhost:8080/oauth2/authorize/kakao?redirect_uri=http://localhost:8080/auth/token")
 	@GetMapping("/token")
-	public ResponseEntity kakaoCallback(@RequestParam String Authorization){
-		System.out.println(Authorization);
-
-
-		return new ResponseEntity(new ResponseEntity<>("성공적으로 카카오 로그인 API 코드를 불러왔습니다.", HttpStatus.OK), HttpStatus.OK);
+	public ResponseEntity<CommonResponse.GeneralResponse> login(){
+		return ResponseEntity.ok().body(responseService.getGeneralResponse(HttpStatus.OK.value(),
+			"실제로는 설명에 있는 주소를 사용합니다. \n (localhost -> 실제 주소"));
 	}
-
-	//
-    @GetMapping("/error")
-    public ResponseEntity errorPage(@RequestParam String error) {
-        return new ResponseEntity(HttpStatus.BAD_REQUEST);
-    }
 }
