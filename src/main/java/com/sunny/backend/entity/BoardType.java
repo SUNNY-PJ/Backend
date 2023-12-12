3ea12aeb5d6328aeb694410fb76f356a911910ef
tkfdkskarl56 <tkfdkskarl56@naver.com>
2023-12-13 00:46:35 +0900

    [feat] : board type 추가 및 예외처리 추가

--- src/main/java/com/sunny/backend/entity/BoardType.java
+++ src/main/java/com/sunny/backend/entity/BoardType.java
@@ -1,9 +1,5 @@
 package com.sunny.backend.entity;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-
-
 public enum BoardType {
-    자유, 꿀팁
+	자유, 꿀팁, 절약
 }
