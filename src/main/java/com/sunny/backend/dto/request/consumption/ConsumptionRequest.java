394a6ebc0e60688ba19ce7e87f408ded0ed67d50
tkfdkskarl56 <tkfdkskarl56@naver.com>
2023-12-19 23:41:28 +0900

    [Refac] : Consumption 등록 수정

--- src/main/java/com/sunny/backend/dto/request/consumption/ConsumptionRequest.java
+++ src/main/java/com/sunny/backend/dto/request/consumption/ConsumptionRequest.java
@@ -1,27 +1,17 @@
 package com.sunny.backend.dto.request.consumption;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.sunny.backend.entity.SpendType;
 import lombok.Getter;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.Date;
 
 @Getter
-@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ConsumptionRequest {
-
     private String name; //지출명
-    private String category; //지출 장소
+    private SpendType category; //지출 장소
     private Long money; //지출 금액
-
     private LocalDate dateField; // 지출 일자
-
 }
-
-
-
-
-
