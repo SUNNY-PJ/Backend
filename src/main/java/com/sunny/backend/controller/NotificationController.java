584e49e5ff257115158c84b4e98c2df2f6feae4d
Eom-Ye-rim <78583768+Eom-Ye-rim@users.noreply.github.com>
2023-12-28 18:55:06 +0900

    Update NotificationController.java

--- src/main/java/com/sunny/backend/controller/NotificationController.java
+++ src/main/java/com/sunny/backend/controller/NotificationController.java
@@ -1,24 +1,17 @@
 package com.sunny.backend.controller;
 
-import java.io.IOException;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.sunny.backend.common.CommonResponse;
 import com.sunny.backend.dto.request.FcmRequestDto;
 import com.sunny.backend.dto.response.NotificationResponse;
 import com.sunny.backend.service.NotificationService;
-
 import io.swagger.annotations.ApiOperation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
-@Tag(name = "9. Alarm", description = "Alarm API")
 @RequiredArgsConstructor
 @RequestMapping("/alarm")
 public class NotificationController {
@@ -28,6 +21,4 @@ public class NotificationController {
     public ResponseEntity<CommonResponse.SingleResponse<NotificationResponse>> pushNotification(@RequestBody FcmRequestDto fcmRequestDto) throws IOException {
         return notificationService.sendPushNotification(fcmRequestDto);
     }
-
-
 }
