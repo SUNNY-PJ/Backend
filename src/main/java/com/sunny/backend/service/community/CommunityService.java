345e1e32d184f110e394ae83efd1367ac85c1622
dpfladlwkdrn@naver.com <dpfladlwkdrn@naver.com>
2023-12-29 17:39:27 +0900

    [update] 친구 프로필 조회 API 수정

--- src/main/java/com/sunny/backend/service/community/CommunityService.java
+++ src/main/java/com/sunny/backend/service/community/CommunityService.java
@@ -222,45 +222,4 @@ public class CommunityService {
 	}
 
 
-	@Transactional
-	public ResponseEntity<CommonResponse.SingleResponse<ProfileResponse>> getUserProfile(
-			CustomUserPrincipal customUserPrincipal, Long communityId) {
-
-		Community community = communityRepository.findById(communityId)
-				.orElseThrow(() -> new CustomException(COMMUNITY_NOT_FOUND));
-
-		return responseService.getSingleResponse(
-				HttpStatus.OK.value(), new ProfileResponse(community.getUsers().getId(),community.getUsers().getName(),community.getUsers().getProfile()),
-				"게시글을 성공적으로 불러왔습니다.");
-	}
-
-	@Transactional
-	public ResponseEntity<CommonResponse.ListResponse<CommunityResponse.PageResponse>> getFriendsCommunity (CustomUserPrincipal customUserPrincipal, Long communityId) {
-
-		Community community = communityRepository.findById(communityId)
-				.orElseThrow(() -> new CustomException(COMMUNITY_NOT_FOUND));
-
-		List<Community> communityList = communityRepository.findAllByUsers_Id(community.getUsers().getId());
-		List<CommunityResponse.PageResponse> communityRes = new ArrayList<>();
-
-		for (Community communities : communityList) {
-			communityRes.add(new CommunityResponse.PageResponse(communities));
-		}
-
-
-		return responseService.getListResponse(HttpStatus.OK.value(), communityRes, "친구가 쓴 작성글 조회");
-	}
-
-	public ResponseEntity<CommonResponse.ListResponse<CommentResponse.Mycomment>> getCommentByFriendsId(CustomUserPrincipal customUserPrincipal,Long communityId) {
-		Community community = communityRepository.findById(communityId)
-				.orElseThrow(() -> new CustomException(COMMUNITY_NOT_FOUND));
-		List<Comment> commentList = commentRepository.findAllByUsers_Id(community.getUsers().getId());
-		List<CommentResponse.Mycomment> commentDTOList =
-				commentList.stream()
-						.map(comment -> new CommentResponse.Mycomment(comment.getCommunity().getId(),comment.getId(), comment.getContent(), comment.getWriter(),comment.getCreatedDate(),comment.getUpdatedDate()))
-						.collect(Collectors.toList());
-
-		return responseService.getListResponse(HttpStatus.OK.value(), commentDTOList, "친구가 쓴 댓글 조회");
-	}
-
 }
