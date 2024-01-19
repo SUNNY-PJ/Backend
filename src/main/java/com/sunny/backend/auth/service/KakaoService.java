9f043a5b71e5519afbfb99dc01e3777778716431
tkfdkskarl56 <tkfdkskarl56@naver.com>
2024-01-20 01:02:05 +0900

    feat : 로그아웃 및 탈퇴 추가

--- src/main/java/com/sunny/backend/auth/service/KakaoService.java
+++ src/main/java/com/sunny/backend/auth/service/KakaoService.java
@@ -2,20 +2,21 @@ package com.sunny.backend.auth.service;
 
 import static com.sunny.backend.common.CommonErrorCode.NICKNAME_IN_USE;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.nimbusds.jose.shaded.json.JSONObject;
 import com.nimbusds.jose.shaded.json.parser.JSONParser;
-import com.nimbusds.jose.shaded.json.parser.ParseException;
 import com.sunny.backend.auth.dto.TokenResponse;
 import com.sunny.backend.auth.dto.UserNameResponse;
 import com.sunny.backend.auth.jwt.TokenProvider;
 import com.sunny.backend.auth.jwt.CustomUserPrincipal;
 import com.sunny.backend.common.CommonCustomException;
 import com.sunny.backend.auth.dto.OAuthToken;
+import com.sunny.backend.common.response.CommonResponse;
 import com.sunny.backend.user.domain.Role;
 import com.sunny.backend.user.domain.Users;
 import com.sunny.backend.user.repository.UserRepository;
+import com.sunny.backend.util.RedisUtil;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,26 +27,26 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Optional;
 
-
+import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class KaKaoService {
+public class KakaoService {
     @Value("${custom_oauth2.client_id}")
-    private String client_id;
+    private String clientId;
     @Value("${custom_oauth2.redirect_uri}")
-    private String redirect_uri;
+    private String redirectUri;
 
+    private final RedisUtil redisUtil;
     private final TokenProvider tokenProvider;
     private final UserRepository userRepository;
 
-    public TokenResponse getAccessToken(String code) throws Exception {
+    public String getAccessToken(String code) {
         RestTemplate rt = new RestTemplate();
 
         // HTTP POST를 요청할 때 보내는 데이터(body)를 설명해주는 헤더도 만들어 같이 보내줘야 한다.
@@ -56,8 +57,8 @@ public class KaKaoService {
         // body는 보통 key, value의 쌍으로 이루어지기 때문에 자바에서 제공해주는 MultiValueMap 타입을 사용한다.
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
-        params.add("client_id", client_id);
-        params.add("redirect_uri", redirect_uri);
+        params.add("client_id", clientId);
+        params.add("redirect_uri", redirectUri);
         params.add("code", code);
 
         // 요청하기 위해 헤더(Header)와 데이터(Body)를 합친다.
@@ -65,20 +66,18 @@ public class KaKaoService {
         HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
 
         // POST 방식으로 Http 요청한다. 그리고 response 변수의 응답 받는다.
-        ResponseEntity<String> response = rt.exchange(
+        ResponseEntity<OAuthToken> response = rt.exchange(
                 "https://kauth.kakao.com/oauth/token",
                 HttpMethod.POST, // 요청할 방식
                 kakaoTokenRequest, // 요청할 때 보낼 데이터
-                String.class // 요청 시 반환되는 데이터 타입
+            OAuthToken.class // 요청 시 반환되는 데이터 타입
         );
-        ObjectMapper objectMapper = new ObjectMapper();
-        OAuthToken oAuthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
-        String email = getEmailForUserInfo(oAuthToken.getAccess_token());
 
-        return tokenProvider.createToken(email, "ROLE_USER");
+        return response.getBody().getAccessToken();
     }
 
-    public String getEmailForUserInfo(String accessToken) throws Exception {
+    public TokenResponse getEmailForUserInfo(String code) throws Exception {
+        String accessToken = getAccessToken(code);
         String host = "https://kapi.kakao.com/v2/user/me";
         URL url = new URL(host);
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
@@ -115,18 +114,24 @@ public class KaKaoService {
                 userRepository.save(users);
             }
         }
-        return email;
+
+        return tokenProvider.createToken(email, "ROLE_USER");
     }
 
+    @Transactional
     public UserNameResponse changeNickname(CustomUserPrincipal customUserPrincipal, String name){
         Users user = customUserPrincipal.getUsers();
-        Users existingUser = userRepository.findByName(name);
-        if (existingUser != null && !existingUser.getId().equals(user.getId())) {
+        Optional<Users> optionalUsers = userRepository.findByName(name);
+        if (optionalUsers.isPresent()) {
             throw new CommonCustomException(NICKNAME_IN_USE);
         }
         user.setName(name);
-        userRepository.save(user);
-
         return new UserNameResponse(user.getName());
+    }
+
+    @Transactional
+    public void leave(CustomUserPrincipal customUserPrincipal) {
+        Users users = customUserPrincipal.getUsers();
+        userRepository.deleteById(users.getId());
     }
 }
