248e04d5fe0c00e5a7ddc0f08443fa0c85b1db5e
dnd <tkfdkskarl56@naver.com>
2024-01-20 18:48:17 +0900

    update : kakao 로그아웃 및 탈퇴

---
+++ src/main/java/com/sunny/backend/common/ComnConstant.java
@@ -0,0 +1,6 @@
+package com.sunny.backend.common;
+
+public class ComnConstant {
+    public static String KAKAO_LOGOUT_URL = "https://kapi.kakao.com/v1/user/logout";
+    public static String KAKAO_LEAVE_URL = "https://kapi.kakao.com/v1/user/unlink";
+}
