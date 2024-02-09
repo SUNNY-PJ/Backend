aa22eafe2921e7350239a97307c98c4f195dc473
tkfdkskarl56 <tkfdkskarl56@naver.com>
2024-02-10 04:14:33 +0900

    update : chat 수정중

--- src/main/java/com/sunny/backend/chat/dto/response/ChatRoomResponse.java
+++ src/main/java/com/sunny/backend/chat/dto/response/ChatRoomResponse.java
@@ -2,12 +2,26 @@ package com.sunny.backend.chat.dto.response;
 
 import com.sunny.backend.chat.domain.ChatUser;
 
-public record ChatRoomResponse (
+public record ChatRoomResponse(
 	Long chatRoomId,
 	Long userFriendId,
 	String friendName,
 	String friendProfile
-){
+	// int notReadCount,
+	// String lastMessage,
+	// LocalDateTime lastMessageTime,
+) {
+	// public ChatRoomResponse(ChatUser chatUser, ChatMessage chatMessage) {
+	// 	this.chatRoomId = chatUser.getChatRoom().getId();
+	// 	this.userFriendId = chatUser.getFriend().getId();
+	// 	this.friendName = chatUser.getFriend().getName();
+	// 	this.friendProfile = chatUser.getFriend().getProfile();
+	// 	this.notReadCount = chatMessage.getReads().stream()
+	// 		.map(msg -> msg.)
+	// 	this.lastMessage = lastMessage;
+	// 	this.lastMessageTime = lastMessageTime;
+	// }
+
 	public static ChatRoomResponse from(ChatUser chatUser) {
 		return new ChatRoomResponse(
 			chatUser.getChatRoom().getId(),
--- src/main/java/com/sunny/backend/chat/repository/ChatUserRepository.java
+++ src/main/java/com/sunny/backend/chat/repository/ChatUserRepository.java
@@ -11,9 +11,11 @@ import com.sunny.backend.chat.exception.ChatErrorCode;
 import com.sunny.backend.common.exception.CustomException;
 
 @Repository
-public interface ChatUserRepository extends JpaRepository<ChatUser, Long> {
+public interface ChatUserRepository extends JpaRepository<ChatUser, Long>, ChatUserCustomRepository {
 	Long countByChatRoom_Id(Long id);
+
 	Optional<ChatUser> findByFriend_IdAndUsers_Id(Long friendId, Long userId);
+
 	List<ChatUser> findByUsers_Id(Long userId);
 
 	default ChatUser getById(Long id) {
