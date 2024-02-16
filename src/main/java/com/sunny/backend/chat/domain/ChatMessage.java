1c0087dfdd7387ce487c45696ed85763250d7d07
tkfdkskarl56 <tkfdkskarl56@naver.com>
2024-02-17 01:45:05 +0900

    feat : 채팅 대화 조회 수정

--- src/main/java/com/sunny/backend/chat/domain/ChatMessage.java
+++ src/main/java/com/sunny/backend/chat/domain/ChatMessage.java
@@ -1,8 +1,5 @@
 package com.sunny.backend.chat.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,7 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 import com.sunny.backend.common.BaseTime;
 import com.sunny.backend.user.domain.Users;
@@ -41,16 +37,14 @@ public class ChatMessage extends BaseTime {
 	@JoinColumn(name = "chat_room_id")
 	private ChatRoom chatRoom;
 
-	@OneToMany(mappedBy = "chatMessage")
-	private final List<ChatRead> reads = new ArrayList<>();
+	@Column
+	private int read;
 
 	public ChatMessage(String message, Users users, ChatRoom chatRoom) {
 		this.message = message;
 		this.users = users;
 		this.chatRoom = chatRoom;
+		this.read = 1;
 	}
 
-	public void addRead(ChatRead chatRead) {
-		this.reads.add(chatRead);
-	}
 }
--- src/main/java/com/sunny/backend/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/sunny/backend/chat/repository/ChatMessageRepository.java
@@ -1,10 +1,16 @@
 package com.sunny.backend.chat.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.sunny.backend.chat.domain.ChatMessage;
 
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long>, ChatMessageRepositoryCustom {
+	@Modifying
+	@Query(value = "UPDATE chat_message cm SET cm.read = 0 where cm.id = :id ", nativeQuery = true)
+	void readMessage(@Param(value = "id") Long id);
 }
