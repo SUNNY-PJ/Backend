bacd8bdfa785ca78cdc579772e4d78d1cc2752a7
tkfdkskarl56 <tkfdkskarl56@naver.com>
2024-02-19 23:44:01 +0900

    feat : userId 추가

--- src/main/java/com/sunny/backend/user/dto/response/UserCommentResponse.java
+++ src/main/java/com/sunny/backend/user/dto/response/UserCommentResponse.java
@@ -8,17 +8,19 @@ import com.sunny.backend.comment.domain.Comment;
 import lombok.Builder;
 
 @Builder
-public record UserCommentResponse (
+public record UserCommentResponse(
 	Long communityId,
+	Long userId,
 	Long commentId,
 	String content,
 	String writer,
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
 	LocalDateTime createdDate
-){
+) {
 	public static UserCommentResponse from(Comment comment) {
 		return UserCommentResponse.builder()
 			.communityId(comment.getCommunity().getId())
+			.userId(comment.getUsers().getId())
 			.commentId(comment.getId())
 			.content(comment.getContent())
 			.writer(comment.getUsers().getNickname())
