b767fa4147099835e3009e3438de3b4e488ce4ad
dpfladlwkdrn@naver.com <dpfladlwkdrn@naver.com>
2024-02-21 16:06:02 +0900

    [update] save API 추가 & 수정

--- src/main/java/com/sunny/backend/save/service/SaveService.java
+++ src/main/java/com/sunny/backend/save/service/SaveService.java
@@ -1,16 +1,11 @@
 package com.sunny.backend.save.service;
 
-import static com.sunny.backend.save.exception.SaveErrorCode.*;
-
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.sunny.backend.auth.jwt.CustomUserPrincipal;
-import com.sunny.backend.common.exception.CustomException;
 import com.sunny.backend.common.response.CommonResponse;
 import com.sunny.backend.common.response.ResponseService;
 import com.sunny.backend.consumption.repository.ConsumptionRepository;
@@ -26,97 +21,78 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 @RequiredArgsConstructor
 public class SaveService {
+
 	private final SaveRepository saveRepository;
 	private final ResponseService responseService;
 	private final ConsumptionRepository consumptionRepository;
 
+	@Transactional
 	public ResponseEntity<CommonResponse.SingleResponse<SaveResponse>> createSaveGoal(
-		CustomUserPrincipal customUserPrincipal, SaveRequest saveRequest) {
+			CustomUserPrincipal customUserPrincipal, SaveRequest saveRequest) {
 		Users user = customUserPrincipal.getUsers();
-		Save save = Save.builder()
-			.cost(saveRequest.getCost())
-				.success(false)
-				.expire(false)
-			.startDate(saveRequest.getStartDate())
-			.endDate(saveRequest.getEndDate())
-			.users(user)
-			.build();
-		saveRepository.save(save);
-		user.addSave(save);
-
-		return responseService.getSingleResponse(HttpStatus.OK.value(), SaveResponse.from(save), "절약 목표를 등록했습니다.");
+		List<Save> saves = saveRepository.findAllByUsers_Id(user.getId());
+		boolean allSavesExpired = saves.stream().allMatch(save -> save.checkExpired(save.getEndDate()));
+		if (allSavesExpired) {
+			Save save = Save.builder()
+					.cost(saveRequest.getCost())
+					.startDate(saveRequest.getStartDate())
+					.endDate(saveRequest.getEndDate())
+					.users(user)
+					.build();
+			saveRepository.save(save);
+			user.addSave(save);
+			return responseService.getSingleResponse(HttpStatus.OK.value(), SaveResponse.from(save,true),
+					"절약 목표를 등록했습니다.");
+		} else {
+			if (!saves.isEmpty()) {
+				Save lastSave = saves.get(saves.size() - 1);
+				SaveResponse saveResponse = SaveResponse.from(lastSave,checkSuccessed(user,lastSave));
+				return responseService.getSingleResponse(HttpStatus.OK.value(), saveResponse, "이미 등록된 절약 목표가 존재합니다.");
+			} else {
+				return responseService.getSingleResponse(HttpStatus.OK.value(), null, "등록된 절약 목표를 찾을 수 없습니다.");
+			}
+		}
 	}
 
 	public ResponseEntity<CommonResponse.SingleResponse<SaveResponse>> updateSaveGoal(
 		CustomUserPrincipal customUserPrincipal, SaveRequest saveRequest) {
 		Users user = customUserPrincipal.getUsers();
-		Save save = saveRepository.findByUsers_Id(user.getId())
-			.orElseThrow(() -> new CustomException(SAVE_NOT_FOUND));
-		save.updateSave(saveRequest);
-		return responseService.getSingleResponse(HttpStatus.OK.value(), SaveResponse.from(save), "절약 목표를 수정했습니다.");
+		List<Save> saves = saveRepository.findAllByUsers_Id(user.getId());
+		Save lastSave = saves.get(saves.size() - 1);
+		lastSave.updateSave(saveRequest);
+		boolean success=checkSuccessed(user,lastSave);
+		return responseService.getSingleResponse(HttpStatus.OK.value(), SaveResponse.from(lastSave,success), "절약 목표를 수정했습니다.");
 	}
 
-	public ResponseEntity<CommonResponse.SingleResponse<SaveResponse.DetailSaveResponse>> getSaveGoal(
-		CustomUserPrincipal customUserPrincipal) {
-		Users user = customUserPrincipal.getUsers();
-		Save save = saveRepository.findByUsers_Id(user.getId())
-			.orElseThrow(() -> new CustomException(SAVE_NOT_FOUND));
-
-		long remainingDays = save.calculateRemainingDays(save);
-		Long userMoney = consumptionRepository.getComsumptionMoney(user.getId(), save.getStartDate(),
-			save.getEndDate());
-		double percentageUsed = save.calculateSavePercentage(userMoney, save);
-		SaveResponse.DetailSaveResponse saveResponse = SaveResponse.DetailSaveResponse.of(remainingDays,
-			percentageUsed);
-		return responseService.getSingleResponse(HttpStatus.OK.value(), saveResponse,
-			"절약 목표를 성공적으로 조회했습니다.");
-	}
-
-	public ResponseEntity<CommonResponse.SingleResponse<SaveResponse>> getDetailSaveGoal(
+	@Transactional
+	public ResponseEntity<CommonResponse.ListResponse<SaveResponse.DetailSaveResponse>> getSaveGoal(
 			CustomUserPrincipal customUserPrincipal) {
 		Users user = customUserPrincipal.getUsers();
-		Save save = saveRepository.findByUsers_Id(user.getId())
-				.orElseThrow(() -> new CustomException(SAVE_NOT_FOUND));
-		boolean expire=true;
-		boolean success=false;
-		long remainingDays = save.calculateRemainingDays(save);
-		Long userMoney = consumptionRepository.getComsumptionMoney(user.getId(), save.getStartDate(),
-				save.getEndDate());
-		double percentageUsed = save.calculateSavePercentage(userMoney, save);
-		if(remainingDays>0) {
-			System.out.println("왜 호출됨");
-			expire=false;
-		}
-		else if(percentageUsed>0){
-			success=true;
-		}
-		save.updateSuccessAndExpire(expire,success);
-		SaveResponse saveResponse=SaveResponse.from(save);
-		return responseService.getSingleResponse(HttpStatus.OK.value(), saveResponse,
+		List<Save> saves = saveRepository.findAllByUsers_Id(user.getId());
+		List<SaveResponse.DetailSaveResponse> saveResponses = saves.stream().map(save -> {
+			long remainingDays = save.calculateRemainingDays(save);
+			Long userMoney = consumptionRepository.getComsumptionMoney(user.getId(), save.getStartDate(), save.getEndDate());
+			double percentageUsed = save.calculateSavePercentage(userMoney,save);
+			return SaveResponse.DetailSaveResponse.of(remainingDays, percentageUsed,save.getCost());
+		}).toList();
+		return responseService.getListResponse(HttpStatus.OK.value(), saveResponses,
+				"절약 목표를 성공적으로 조회했습니다.");
+	}
+	public ResponseEntity<CommonResponse.ListResponse<SaveResponse>> getDetailSaveGoal(
+			CustomUserPrincipal customUserPrincipal) {
+		Users user = customUserPrincipal.getUsers();
+		List<Save> saves = saveRepository.findAllByUsers_Id(user.getId());
+		List<SaveResponse> saveResponses = saves.stream().map(save -> {
+			return SaveResponse.from(save,checkSuccessed(user,save));
+		}).toList();
+		return responseService.getListResponse(HttpStatus.OK.value(), saveResponses,
 				"절약 목표 성공적으로 조회했습니다.");
 	}
 
-	public ResponseEntity<CommonResponse.SingleResponse<SaveResponse>> getEndSaveGoal(
-			CustomUserPrincipal customUserPrincipal) {
-		Users user = customUserPrincipal.getUsers();
-		Save save = saveRepository.findByUsers_Id(user.getId())
-				.orElseThrow(() -> new CustomException(SAVE_NOT_FOUND));
-		boolean expire=false;
-		boolean success=false;
-		long remainingDays = save.calculateRemainingDays(save);
-		Long userMoney = consumptionRepository.getComsumptionMoney(user.getId(), save.getStartDate(),
-				save.getEndDate());
-		double percentageUsed = save.calculateSavePercentage(userMoney, save);
-		if(remainingDays<0 && percentageUsed<0) {
-			expire=true;
-		}
-		if(percentageUsed>0){
-			success=true;
-		}
-		save.updateSuccessAndExpire(expire,success);
-		SaveResponse saveResponse=SaveResponse.from(save);
-		return responseService.getSingleResponse(HttpStatus.OK.value(), saveResponse,
-				"절약 목표 성공적으로 조회했습니다.");
+	public boolean checkSuccessed(Users user,Save save) {
+		Long userMoney = consumptionRepository.getComsumptionMoney(user.getId(), save.getStartDate(), save.getEndDate());
+		double percentageUsed = save.calculateSavePercentage(userMoney,save);
+		return percentageUsed >= 0;
 	}
 
 }
