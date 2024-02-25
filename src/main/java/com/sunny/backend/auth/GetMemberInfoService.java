6773299c32d3b79c7bc7a5620941399e5f49968e
dpfladlwkdrn@naver.com <dpfladlwkdrn@naver.com>
2024-02-25 20:58:48 +0900

    [feat] apple login test

---
+++ src/main/java/com/sunny/backend/auth/GetMemberInfoService.java
@@ -0,0 +1,60 @@
+//package com.sunny.backend.auth;
+//
+//import com.sunny.backend.auth.dto.AppleAuthClient;
+//import io.jsonwebtoken.JwsHeader;
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.SignatureAlgorithm;
+//import java.security.PrivateKey;
+//import java.security.Security;
+//import java.time.LocalDateTime;
+//import java.time.ZoneId;
+//import java.util.Base64;
+//import java.util.Date;
+//import lombok.RequiredArgsConstructor;
+//import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+//import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class GetMemberInfoService {
+//  private final AppleAuthClient appleAuthClient;
+//  private final AppleProperties appleProperties;
+//
+//  public AppleIdTokenPayload get(String authorizationCode){
+//    String idToken=appleAuthClient.getIdToken(
+//        appleProperties.getClientId(),
+//        generateClientSecret(),
+//        appleProperties.getGrantType(),
+//        authorizationCode
+//    ).getIdToken();
+//    return TokenDecoder.decodePayload(idToken,AppleIdTokenPayload.class);
+//  }
+//
+//
+//  private String generateClientSecret(){
+//    LocalDateTime expiration=LocalDateTime.now().plusMinutes(5);
+//    return Jwts.builder()
+//        .setHeaderParam(JwsHeader.KEY_ID, appleProperties.getKeyId())
+//        .setIssuer(appleProperties.getTeamId())
+//        .setAudience(appleProperties.getAudience())
+//        .setSubject(appleProperties.getClientId())
+//        .setExpiration(Date.from(expiration.atZone(ZoneId.systemDefault()).toInstant()))
+//        .setIssuedAt(new Date())
+//        .signWith(SignatureAlgorithm.ES256, getPrivateKey())
+//        .compact();
+//  }
+//
+//  private PrivateKey getPrivateKey() {
+//    Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+//    JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
+//    try {
+//      byte[] privateKeyBytes = Base64.getDecoder().decode(appleProperties.getPrivateKey());
+//
+//      PrivateKeyInfo privateKeyInfo = PrivateKeyInfo.getInstance(privateKeyBytes);
+//      return converter.getPrivateKey(privateKeyInfo);
+//    } catch (Exception e) {
+//      throw new RuntimeException("Error converting private key from String", e);
+//    }
+//  }
+//}
---
+++ src/main/java/com/sunny/backend/auth/service/AppleService.java
@@ -0,0 +1,11 @@
+package com.sunny.backend.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+// TODO 초기 설정항 값 yml file에 넣기
+public class AppleService {
+
+}
