88bf164c492b45129e253f88bee3626e0bf11bdb
dpfladlwkdrn@naver.com <dpfladlwkdrn@naver.com>
2024-03-21 22:45:12 +0900

    [chore] logout test ver 3

--- src/main/java/com/sunny/backend/auth/jwt/TokenProvider.java
+++ src/main/java/com/sunny/backend/auth/jwt/TokenProvider.java
@@ -1,16 +1,10 @@
 package com.sunny.backend.auth.jwt;
 
-import com.sunny.backend.auth.dto.UserRequest;
-import com.sunny.backend.common.response.ResponseService;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -25,7 +19,6 @@ import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.SignatureException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.util.ObjectUtils;
 
 
 @Service
@@ -38,29 +31,23 @@ public class TokenProvider {
 
 	private final RedisUtil redisUtil;
 	private final CustomUserDetailsService customUserDetailsService;
-	private ResponseService responseService;
-	private RedisTemplate redisTemplate;
 
 	public TokenResponse createToken(String email, String authorities,boolean isUserRegistered) {
 		String accessToken = Jwts.builder()
-			.setHeader(createHeader())
-			.claim("email", email)
-			.claim("authorities", authorities)
-			.setSubject("accessToken")
-			.setExpiration(createExpiredDate(1))
-			.signWith(SignatureAlgorithm.HS512, tokenSecret)
-			.compact();
+				.setHeader(createHeader())
+				.claim("email", email)
+				.claim("authorities", authorities)
+				.setSubject("accessToken")
+				.setExpiration(createExpiredDate(1))
+				.signWith(SignatureAlgorithm.HS512, tokenSecret)
+				.compact();
 		System.out.println("accessToken "+accessToken);
 		String refreshToken = Jwts.builder()
-			.setHeader(createHeader())
-			.setSubject("refreshToken")
-			.setExpiration(createExpiredDate(2 * 7 * 24))
-			.signWith(SignatureAlgorithm.HS512, tokenSecret)
-			.compact();
-		Authentication authentication = getAuthentication(accessToken);
-//		//TODO test용
-//		redisTemplate.opsForValue()
-//				.set("RT:" + authentication.getName(), refreshToken, getClaims(refreshToken).getExpiration().getTime(), TimeUnit.MILLISECONDS);
+				.setHeader(createHeader())
+				.setSubject("refreshToken")
+				.setExpiration(createExpiredDate(2 * 7 * 24))
+				.signWith(SignatureAlgorithm.HS512, tokenSecret)
+				.compact();
 
 		redisUtil.setValuesWithTimeout(refreshToken, email, getClaims(refreshToken).getExpiration().getTime());
 
@@ -69,9 +56,9 @@ public class TokenProvider {
 
 	public Claims getClaims(String token) {
 		return Jwts.parser()
-			.setSigningKey(tokenSecret)
-			.parseClaimsJws(token)
-			.getBody();
+				.setSigningKey(tokenSecret)
+				.parseClaimsJws(token)
+				.getBody();
 	}
 
 	public boolean validateToken(String token) {
@@ -109,59 +96,4 @@ public class TokenProvider {
 		CustomUserPrincipal userDetails = customUserDetailsService.loadUserByUsername(email);
 		return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 	}
-
-	public ResponseEntity<?> logout(UserRequest logout) {
-		if (!validateToken(logout.getAccessToken())) {
-			return responseService.getGeneralResponse(HttpStatus.BAD_REQUEST.value(), "잘못된 요청입니다.");
-		}
-		Authentication authentication = getAuthentication(logout.getAccessToken());
-
-		if (redisTemplate.opsForValue().get("RT:" + authentication.getName()) != null) {
-			redisTemplate.delete("RT:" + authentication.getName());
-		}
-		//blacklist 처리
-		Long expiration = getExpiration(logout.getAccessToken());
-		redisTemplate.opsForValue()
-				.set(logout.getAccessToken(), "logout", expiration, TimeUnit.MILLISECONDS);
-
-		return responseService.getGeneralResponse(HttpStatus.OK.value(), "로그아웃 성공입니다.");
-	}
-	public Long getExpiration(String accessToken) {
-		// accessToken 남은 유효시간
-		Date expiration =
-				Jwts.parser()
-						.setSigningKey(tokenSecret)
-						.parseClaimsJws(accessToken)
-						.getBody().getExpiration();
-		// 현재 시간
-		Long now = new Date().getTime();
-		return (expiration.getTime() - now);
-	}
-	public ResponseEntity<?> reissue(UserRequest reissue,String email) {
-		// Refresh Token 검증
-		if (!validateToken(reissue.getRefreshToken())) {
-			return responseService.getGeneralResponse(HttpStatus.BAD_REQUEST.value(), "Refresh Token 정보가 유효하지 않습니다.");
-		}
-		Authentication authentication = getAuthentication(reissue.getAccessToken());
-
-		// Redis 에서 User email 을 기반으로 저장된 Refresh Token 값
-		String refreshToken = (String)redisTemplate.opsForValue().get("RT:" + authentication.getName());
-		// 로그아웃되어 Redis에 RefreshToken 이 존재하지 않는 경우 처리
-		if(ObjectUtils.isEmpty(refreshToken)) {
-			return responseService.getGeneralResponse(HttpStatus.BAD_REQUEST.value(), "잘못된 요청입니다.");
-		}
-		if(!refreshToken.equals(reissue.getRefreshToken())) {
-			return responseService.getGeneralResponse(HttpStatus.BAD_REQUEST.value(), "RefreshToken 정보가 잘못되었습니다.");
-		}
-
-		// 토큰 생성
-		TokenResponse tokenInfo = createToken(email, authentication.getAuthorities().toString(),true);
-
-		//RefreshToken Redis 업데이트
-		redisTemplate.opsForValue()
-				.set("RT:" + authentication.getName(), tokenInfo.refreshToken(),getClaims(tokenInfo.refreshToken()).getExpiration().getTime(), TimeUnit.MILLISECONDS);
-
-		return responseService.getSingleResponse(HttpStatus.OK.value(), tokenInfo,"토큰 정복 갱신되었습니다.");
-	}
-
 }
