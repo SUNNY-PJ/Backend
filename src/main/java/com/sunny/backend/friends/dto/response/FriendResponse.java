3165f739fbde0e16b4f06036d01806c32a07944d
tkfdkskarl56 <tkfdkskarl56@naver.com>
2024-03-25 00:21:18 +0900

    hotfix: 친구 목록 response

--- src/main/java/com/sunny/backend/friends/dto/response/FriendResponse.java
+++ src/main/java/com/sunny/backend/friends/dto/response/FriendResponse.java
@@ -6,19 +6,19 @@ import com.sunny.backend.friends.domain.FriendStatus;
 import lombok.Builder;
 
 @Builder
-public record FriendResponse (
+public record FriendResponse(
 	Long friendId,
 	Long userFriendId,
 	String nickname,
 	String profile,
 	FriendStatus friendStatus
-){
+) {
 	public static FriendResponse from(Friend friend) {
 		return FriendResponse.builder()
 			.friendId(friend.getId())
 			.userFriendId(friend.getUserFriend().getId())
-			.nickname(friend.getUsers().getNickname())
-			.profile(friend.getUsers().getProfile())
+			.nickname(friend.getUserFriend().getNickname())
+			.profile(friend.getUserFriend().getProfile())
 			.friendStatus(friend.getStatus())
 			.build();
 	}
