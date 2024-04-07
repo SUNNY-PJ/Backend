47ad571a5c7e84fa76f27a98de9c857d4ec33ff0
dpfladlwkdrn@naver.com <dpfladlwkdrn@naver.com>
2024-04-07 13:03:42 +0900

    [update] 사용하지 않는 변수 삭제

--- src/main/java/com/sunny/backend/scrap/service/ScrapService.java
+++ src/main/java/com/sunny/backend/scrap/service/ScrapService.java
@@ -42,10 +42,7 @@ public class ScrapService {
 	public ResponseEntity<CommonResponse.GeneralResponse> removeScrapFromCommunity(
 		CustomUserPrincipal customUserPrincipal, Long communityId) {
 		Users user = userRepository.getById(customUserPrincipal.getId());
-		Community community = communityRepository.getById(communityId);
-
-		Scrap scrap = user.findScrapByCommunity(community.getId());
-
+		Scrap scrap = user.findScrapByCommunity(communityId);
 		scrapRepository.delete(scrap);
 		return responseService.getGeneralResponse(HttpStatus.OK.value(), "스크랩 게시글이 삭제 되었습니다.");
 	}
