ba3eec0d20802bd4a5d5a51988f86a53bbe4db22
tkfdkskarl56 <tkfdkskarl56@naver.com>
2024-04-14 12:57:25 +0900

    hotfix : 마이페이지에서 비밀 댓글인데 조회 가능한 상황 수정

--- src/main/java/com/sunny/backend/user/dto/response/UserCommentResponse.java
+++ src/main/java/com/sunny/backend/user/dto/response/UserCommentResponse.java
@@ -21,16 +21,29 @@ public record UserCommentResponse(
 
 	public static UserCommentResponse from(Comment comment, Users currentUser) {
 		String commentContent = "";
-		if (comment.getIsPrivated() && !(currentUser.getId().equals(comment.getUsers().getId()) ||
-			currentUser.getId().equals(comment.getCommunity().getUsers().getId()))) {
-			if (comment.getIsDeleted()) {
-				commentContent = "삭제된 댓글입니다.";
-			} else {
-				commentContent = "비밀 댓글입니다.";
-			}
-		} else {
+
+		if (currentUser.getId().equals(comment.getUsers().getId())
+			|| currentUser.getId().equals(comment.getCommunity().getId())) {
 			commentContent = comment.getContent();
+		} else {
+			if (comment.getIsPrivated()) {
+				commentContent = "비밀 댓글입니다.";
+				if (comment.getIsDeleted()) {
+					commentContent = "삭제된 댓글입니다.";
+				}
+			}
 		}
+		//
+		// if (comment.getIsPrivated() && (currentUser.getId().equals(comment.getUsers().getId()) ||
+		// 	currentUser.getId().equals(comment.getCommunity().getUsers().getId()))) {
+		// 	if (comment.getIsDeleted()) {
+		// 		commentContent = "삭제된 댓글입니다.";
+		// 	} else {
+		// 		commentContent = "비밀 댓글입니다.";
+		// 	}
+		// } else {
+		// 	commentContent = comment.getContent();
+		// }
 
 		return UserCommentResponse.builder()
 			.communityId(comment.getCommunity().getId())
