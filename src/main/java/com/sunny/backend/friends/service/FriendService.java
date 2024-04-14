e7a6fdca89bd6136ee53b1954109049bddcd9a36
tkfdkskarl56 <tkfdkskarl56@naver.com>
2024-04-15 00:59:15 +0900

    hotfix : 친구 삭제 API 수정

--- src/main/java/com/sunny/backend/friends/service/FriendService.java
+++ src/main/java/com/sunny/backend/friends/service/FriendService.java
@@ -2,8 +2,11 @@ package com.sunny.backend.friends.service;
 
 import static com.sunny.backend.friends.exception.FriendErrorCode.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
 
@@ -11,12 +14,19 @@ import org.springframework.stereotype.Service;
 
 import com.sunny.backend.auth.jwt.CustomUserPrincipal;
 import com.sunny.backend.common.exception.CustomException;
+import com.sunny.backend.competition.repository.CompetitionRepository;
 import com.sunny.backend.friends.domain.Friend;
+import com.sunny.backend.friends.domain.FriendCompetition;
+import com.sunny.backend.friends.domain.FriendCompetitionStatus;
 import com.sunny.backend.friends.domain.FriendStatus;
 import com.sunny.backend.friends.dto.response.FriendCompetitionDto;
+import com.sunny.backend.friends.dto.response.FriendCompetitionResponse;
 import com.sunny.backend.friends.dto.response.FriendListResponse;
+import com.sunny.backend.friends.dto.response.FriendResponse;
 import com.sunny.backend.friends.repository.FriendCompetitionRepository;
 import com.sunny.backend.friends.repository.FriendRepository;
+import com.sunny.backend.notification.repository.CompetitionNotificationRepository;
+import com.sunny.backend.notification.repository.FriendsNotificationRepository;
 import com.sunny.backend.notification.service.FriendNotiService;
 import com.sunny.backend.user.domain.Users;
 import com.sunny.backend.user.repository.UserRepository;
@@ -31,11 +41,32 @@ public class FriendService {
 	private final FriendCompetitionRepository friendCompetitionRepository;
 	private final UserRepository userRepository;
 	private final FriendNotiService friendNotiService;
+	private final CompetitionNotificationRepository competitionNotificationRepository;
+	private final CompetitionRepository competitionRepository;
+	private final FriendsNotificationRepository friendsNotificationRepository;
 
 	public FriendListResponse getFriends(CustomUserPrincipal customUserPrincipal) {
 		Users user = userRepository.getById(customUserPrincipal.getId());
-		List<FriendCompetitionDto> friends = friendCompetitionRepository.getByFriendLeftJoinFriend(user.getId());
-		return FriendListResponse.of(friends);
+		List<FriendCompetitionDto> friendCompetitions = friendCompetitionRepository.getByFriendLeftJoinFriend(user.getId());
+		List<FriendCompetitionResponse> competitions = new ArrayList<>();
+		List<FriendCompetitionResponse> approveList = new ArrayList<>();
+		List<FriendResponse> waitList = new ArrayList<>();
+		for (FriendCompetitionDto friendCompetition : friendCompetitions) {
+			if (friendCompetition.getFriendStatus() == FriendStatus.FRIEND) {
+				if (friendCompetition.getFriendCompetitionStatus() == FriendCompetitionStatus.PROCEEDING) {
+					competitions.add(FriendCompetitionResponse.from(friendCompetition));
+				} else if (friendCompetition.getFriendCompetitionStatus() == FriendCompetitionStatus.SEND
+					|| friendCompetition.getFriendCompetitionStatus() == FriendCompetitionStatus.RECEIVE) {
+					approveList.add(FriendCompetitionResponse.from(friendCompetition));
+				} else {
+					approveList.add(FriendCompetitionResponse.from(friendCompetition));
+				}
+			} else if(friendCompetition.getFriendStatus() == FriendStatus.RECEIVE) {
+				waitList.add(FriendResponse.from(friendCompetition));
+			}
+		}
+
+		return new FriendListResponse(competitions, approveList, waitList);
 	}
 
 	@Transactional
@@ -121,15 +152,23 @@ public class FriendService {
 	@Transactional
 	public void deleteFriends(CustomUserPrincipal customUserPrincipal, Long friendId) {
 		Friend friend = friendRepository.getById(friendId);
+		Users users = friend.getUsers();
 		friend.validateUser(customUserPrincipal.getId());
 
-		// friendCompetitionRepository.deleteAllByFriend(friend);
-		// competitionRepository.de
-		// 상대편 친구 관계 삭제
-		friendRepository.findByUsersAndUserFriend(friend.getUserFriend(), friend.getUsers())
-			.ifPresent(friendRepository::delete);
-		// 나의 친구 관계 삭제
-		friendRepository.delete(friend);
+		List<FriendCompetition> friendCompetitions = friendCompetitionRepository.getByUserOrUserFriend(users.getId());
+		for (FriendCompetition friendCompetition : friendCompetitions) {
+			competitionNotificationRepository.deleteAllByFriendCompetition(friendCompetition);
+			friendCompetitionRepository.deleteById(friendCompetition.getId());
+		}
+		Set<Long> competitionIds = friendCompetitions.stream()
+			.map(friendCompetition -> friendCompetition.getCompetition().getId())
+			.collect(Collectors.toSet());
+		if (!competitionIds.isEmpty()) {
+			competitionRepository.deleteAllById(competitionIds);
+		}
+
+		friendsNotificationRepository.deleteByUsersOrFriend(users, users);
+		friendRepository.deleteByUsersOrUserFriend(users, users);
 	}
 
 }
